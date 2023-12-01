5fc29cfbad9440d5e85702362452e7afcc0e42eb
BlackHatDevX <jashgro@yandex.com>
2023-12-01 21:26:36 +0530

    +reducer

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,36 @@ import AppHeading from "./components/AppHeading";
 import ItemsList from "./components/ItemsList";
 import "./App.css";
 import ItemsInput from "./components/ItemsInput";
-import { useState } from "react";
+import { useReducer } from "react";
 import EmptyMessage from "./components/EmptyMessage";
 import { Database } from "./store/Database";
 
+const todoItemsReducer = (currTodoItems, action) => {
+  if (action.type === "NEW_ITEM") {
+    const newData = [...currTodoItems, action.payload];
+    return newData;
+  } else if (action.type === "DELETE_ITEM") {
+    const newData = currTodoItems.filter((item) => item != action.payload);
+    return newData;
+  }
+};
+
 const App = () => {
-  const [data, setData] = useState([]);
+  const [data, dispatchData] = useReducer(todoItemsReducer, []);
   const newData = "";
   const addData = (newData) => {
-    setData((currData) => [...currData, newData]);
+    const newItemAction = {
+      type: "NEW_ITEM",
+      payload: newData,
+    };
+    dispatchData(newItemAction);
   };
   const deleteData = (itemDeleted) => {
-    const newDelete = data.filter((item) => item != itemDeleted);
-    setData(newDelete);
+    const deleteItemAction = {
+      type: "DELETE_ITEM",
+      payload: itemDeleted,
+    };
+    dispatchData(deleteItemAction);
   };
   return (
     <Database.Provider value={{ data, addData, deleteData }}>
